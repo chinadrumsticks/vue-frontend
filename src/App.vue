bdfd0e5f710922833be5acccf0f2d7ccd3af9fca
Paul Ohrt <paul.ohrt@me.com>
2022-09-03 17:32:42 +0200

    Add homepage carousel with controls

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
   <NavBar></NavBar>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center">
+        <MainCarousel></MainCarousel>
+      </div>
+    </div>
+
+    <div class="row text-start">
+      <div class="col-6 col-md-2 border">
+        Text 1
+      </div>
+      <div class="col-6 col-md-8 border">
+        Text 2
+      </div>
+      <div class="col col-md-2 border">
+        Text 3
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar";
-export default {
-  name: 'App',
-  components: {
-    NavBar
+  import NavBar from "./components/NavBar";
+  import MainCarousel from "./components/MainCarousel.vue";
+  export default {
+    name: 'App',
+    components: {
+      MainCarousel,
+      NavBar
+    }
   }
-}
 </script>
 
 <style>
